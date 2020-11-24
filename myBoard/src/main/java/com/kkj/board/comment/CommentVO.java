8f482623aa6ddd18613322bd956e65e08a897788
osk14741 <66981983+osk14741@users.noreply.github.com>
2020-11-24 15:13:45 +0900

    VO 생성

---
+++ myBoard/src/main/java/com/kkj/board/comment/CommentVO.java
@@ -0,0 +1,8 @@
+package com.kkj.board.comment;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommentVO {
+
+}
---
+++ myBoard/src/main/java/com/kkj/board/workspace/WorkspaceVO.java
@@ -0,0 +1,8 @@
+package com.kkj.board.workspace;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class WorkspaceVO {
+
+}
